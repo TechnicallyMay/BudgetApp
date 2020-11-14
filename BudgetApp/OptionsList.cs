bc22aec0cac8c78044f24e4600e6c91b989480c6
justinwheeler45 <73674706+justinwheeler45@users.noreply.github.com>
2020-11-14 15:22:37 -0700

    Fixed off-by-one error in OptionsList

--- BudgetApp/OptionsList.cs
+++ BudgetApp/OptionsList.cs
@@ -16,14 +16,13 @@ namespace BudgetApp
         {
             for (int i = 1; i <= m_options.Count; i++)
             {
-                Console.WriteLine($"Option {i}. {m_options[i - 1].OptionText}");
+                Console.WriteLine($"{i}. {m_options[i - 1].OptionText}");
             }
 
             string input = Console.ReadLine();
 
-            if (int.TryParse(input, out int intInput) && intInput < m_options.Count && intInput > 0)
+            if (int.TryParse(input, out int intInput) && intInput <= m_options.Count && intInput > 0)
             {
-                    
                 var selectedOption = m_options[intInput - 1];
                 selectedOption.Action();
             }
